fff10ead98362822f4f943fcbe52a19aeb98927a
Binod Regmi <regmibinod30@gmail.com>
2023-08-09 18:19:26 -0400

    network_plot.R edited

--- R/generate_network_plot.R
+++ R/generate_network_plot.R
@@ -13,11 +13,8 @@
 #'
 
 #' @export
-term_vector <- c("Wnt signaling pathway", "FoxO signaling pathway", "Toll-like receptor signaling pathway")
-#network_plot <- function(expanded_enrichr_d=expanded_enrichr_df, term_vector = term_vector)
-#filtered_df <- filter(expanded_enrichr_df$Term %in% term_vector)
-network_plot <- function(expanded_enrichr_df = expanded_enrichr_df, term_vector = term_vector)
+network_plot <- function(expanded_enrichr_df = expanded_enrichr_df, term_vector = term_vector){
   filtered_df <- filter(expanded_enrichr_df, Term %in% term_vector) %>% select(Term, gene)
   A <- get.adjacency(graph.edgelist(as.matrix(filtered_df), directed = FALSE))
   net2 <- graph_from_adjacency_matrix(A, mode = "undirected")
-  plot(net2)
+  plot(net2)}
