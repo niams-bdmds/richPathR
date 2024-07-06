807987b1f47a15dc4219059834212342cbefca80
Pradeep Thapaliya <pradeep.thapaliya95@gmail.com>
2024-07-06 18:36:03 -0400

    extend bar plot to include minimum p value addition to minimum combined score

--- R/bar_plot_count.R
+++ R/bar_plot_count.R
@@ -3,24 +3,34 @@
 #' This bar plot visualizes how many terms (pathways) are distributed
 #' in the gene lists.
 
-#'@param enrichr_df A data frame generated  by implementing the function
+#' @param enrichr_df A data frame generated  by implementing the function
 #' enrichr_df.
 
-#'@param minimum_combined_score A integer value below which the combined score
-#' is set to be insignificant.Default is 5.
+#' @param value A integer value below which the combined score or p value is set to be insignificant.Default is 5.
 
-#'@return A bar plot.
+#' @param value_type A character value specifying the type of value to be used for filtering the significant terms. Default is 'minimum_combined_score'.
 
-#'@example
+#' @return A bar plot.
+
+#' @example
 #' bar_plot_count(enrichr_df = enrichr_df, minimum_combined_score)
 
-#'@export
-bar_plot_count <- function (enrichr_df, minimum_combined_score = 5){
-  filter_significant_terms <- enrichr_df %>%
-    filter(., Combined.Score > minimum_combined_score)
+#' @export
+bar_plot_count <- function(enrichr_df, value = 5, value_type = "minimum_combined_score") {
+  filter_significant_terms <- list()
+  if (value_type == "minimum_combined_score") {
+    filter_significant_terms <- enrichr_df %>% filter(Combined.Score > value)
+  } else if (value_type == "minimum_p_value") {
+    filter_significant_terms <- enrichr_df %>% filter(P.value > value)
+  } else {
+    print("Unknown value type")
+    return
+  }
+
 
   plot <- ggplot(filter_significant_terms, aes(gene_list)) +
     geom_bar(fill = "blue") +
     theme(axis.text.x = element_text(angle = 90)) +
     theme()
-  return(plot)}
+  return(plot)
+}
